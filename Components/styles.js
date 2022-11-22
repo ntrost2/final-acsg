8b90a7202a0dda527d6791c28bdc5bc8c67d09a7
ntrost2 <111935823+ntrost2@users.noreply.github.com>
2022-11-21 22:09:26 -0600

    Update styles.js

---
+++ Components/styles.js
@@ -0,0 +1,11 @@
+const styleGoogleButton = {
+    width: '70px',
+    height: '70px',
+    margin: '0px auto 20px',
+    color: 'red',
+    borderRadius: '3px',
+  };
+  
+  module.exports = {
+     styleGoogleButton,
+  };
